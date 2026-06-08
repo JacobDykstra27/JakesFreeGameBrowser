63d62199d05dfac63b3cd8b9080b170972ec8184
Jacob <dykstraj@my.lanecc.edu>
2026-06-07 20:37:59 -0700

    final tweaks

--- app/(tabs)/game-deals.jsx
+++ app/(tabs)/game-deals.jsx
@@ -11,18 +11,39 @@ import { useWishlist } from "../../hooks/useWishlist";
 const STORE_NAMES = {
 	1: "Steam",
 	2: "GamersGate",
-	3: "GOG",
-	4: "Humble Bundle",
-	5: "Green Man Gaming",
-	6: "Fanatical",
-	7: "GameBillet",
-	8: "WinGameStore",
-	9: "IndieGala",
-	10: "Blizzard",
-	11: "Epic Games",
-	12: "Xbox",
-	13: "PlayStation",
-	14: "Nintendo",
+	3: "GreenManGaming",
+	4: "Amazon",
+	5: "GameStop",
+	6: "Direct2Drive",
+	7: "GOG",
+	8: "Origin",
+	9: "Get Games",
+	10: "Shiny Loot",
+	11: "Humble Store",
+	12: "Desura",
+	13: "Uplay",
+	14: "IndieGameStand",
+	15: "Fanatical",
+	16: "GamesRocket",
+	17: "Games Republic",
+	18: "Sila Games",
+	19: "Playfield",
+	20: "ImperialGames",
+	21: "WinGameStore",
+	22: "FunStockDigital",
+	23: "GameBillet",
+	24: "Voidu",
+	25: "Epic Games Store",
+	26: "Razer Game Store",
+	27: "Gamesplanet",
+	28: "Gamesload",
+	29: "2Game",
+	30: "IndieGala",
+	31: "Blizzard Shop",
+	32: "AllYouPlay",
+	33: "DLGamer",
+	34: "Noctre",
+	35: "DreamGame"
 };
 
 const styles = StyleSheet.create({
@@ -228,7 +249,6 @@ export default function GameDealsScreen() {
 
 	return (
 		<View style={styles.container}>
-			{/* header menu moved to Tabs layout headerRight */}
 			<FlatList
 				data={games}
 				renderItem={({ item }) => (
